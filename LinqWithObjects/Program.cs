dc1d7dc50780bc3cae18db5a10cc1479f85e562d
Alexander Butko <thebadgroover@gmail.com>
2025-07-29 17:16:43 +0300

    test

---
+++ LinqWithObjects/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] names = ["Michael", "Pam", "Kim", "Dwight", "Angel", "Kevin", "Toby", "Creed"];
+
+SectionTitle("Deffered execution");
+
+var query1 = names.Where(name => name.EndsWith("m"));
+
+
+var query2 = from name in names where name.EndsWith("m") select name;
+
